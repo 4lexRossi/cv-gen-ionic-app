4d35c292bf089eeeea2c557c2abd11f36dfa2f66
4lexRossi <devalexrossi@gmail.com>
2024-08-04 13:11:06 -0300

    chore: add capacitor

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.cvgen',
   appName: 'cvGen',
   webDir: 'www'
 };
